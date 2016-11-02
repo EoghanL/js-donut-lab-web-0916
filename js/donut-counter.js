16ae09f8c8f489f92c366ad66941d883e8e04b0f
github username <github email address>
2016-11-02 14:16:20 -0400

    Donuts for days

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -1,4 +1,20 @@
 //JS Donut Counter Lab
+var guests = prompt("Please enter the number of guests here:")
+var donuts = prompt("Please enter the number of donuts here:")
+
+if(parseInt(guests) > parseInt(donuts)){
+  alert(`You have ${donuts} donuts for ${guests} guests you are short ${guests - donuts} donuts!`)
+}
+else if(parseInt(guests) <= parseInt(donuts)){
+  alert("You have enough donuts for everyone, CONGRATS!")
+}
+else{
+  alert("You probably did something wrong")
+}
+
+
+
+
 
 //declare 2 variables one for guests and one for donuts
 //use prompt method to bring up a dialog box in the browser for the user to insert the number of guests and donuts.
